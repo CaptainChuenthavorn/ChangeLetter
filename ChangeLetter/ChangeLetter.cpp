ca6e0529e59340e7f2eccdea6259e000ec345c6f
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-09-11 20:48:31 +0700

    Add project files.

---
+++ ChangeLetter/ChangeLetter.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	char str[50], * p;
+	printf("Program that printf only letter and change lower to capital or change capital to lower \n");
+	printf("Enter string  : ");
+	scanf("%[^\n]s", str);
+	p = str;
+	printf("\n\nThe result is : ");
+	while (*p != '\0')
+	{
+		if ((*p >= 'a' && *p <= 'z') || (*p >= 'A' && *p <= 'Z'))
+		{
+			//method change lower to capital letter
+			if (*p >= 'a' && *p <= 'z') {
+				*p = *p - 'b' + 'B';
+			}
+			//method change capital to lower letter
+			else if (*p >= 'A' && *p <= 'Z') {
+				*p = *p - 'A' + 'a';
+			}
+			printf("%c", *p);
+			p++;
+		}
+		else p++;
+	}
+	printf("\n\n");
+	return 0;
+}
